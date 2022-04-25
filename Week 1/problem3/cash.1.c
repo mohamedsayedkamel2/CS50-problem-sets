f0f391e8238805416a8d264330b3deaac0344bef
mohamed <semsayedkamel7@gmail.com>
2022-04-25 17:05:13 +0200

    problem 3
    Added problem set 3

---
+++ Week 1/problem3/cash.1.c
@@ -0,0 +1,114 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
+int main(void)
+{
+    // Ask how many cents the customer is owed
+    int cents = get_cents();
+
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
+}
+
+int get_cents(void)
+{
+    int n = get_int("Enter the amount of cents: ");
+    //User input validation: checks if the value is positive
+    while (n < 0)
+    {
+    n = get_int("Enter the amount of cents: ");
+    }
+    return n;
+}
+//Calculates the quarters
+int calculate_quarters(int cents)
+{
+    if (cents == 25 || (cents >= 26 && cents <= 49))
+    {
+        return 1;
+    }
+    else if (cents >= 50 && cents <= 74)
+    {
+        return 2;
+    }
+    else if (cents >= 75 && cents <= 99)
+    {
+        return 3;
+    }
+    else if (cents == 100)
+    {
+        return 4;
+    }
+    return 0;
+}
+
+//Calculates the dimes
+int calculate_dimes(int cents)
+{
+    if (cents >= 10 && cents <= 19)
+    {
+        return 1;
+    }
+    else if (cents >= 20 && cents <= 24)
+    {
+        return 2;
+    }
+    return 0;
+}
+
+//Calculates the nickels
+int calculate_nickels(int cents)
+{
+    if (cents >= 5 && cents <= 9)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+//Calculates the pennies
+int calculate_pennies(int cents)
+{
+    if (cents == 1)
+    {
+        return 1;
+    }
+    else if (cents == 2)
+    {
+        return 2;
+    }
+    else if (cents == 3)
+    {
+        return 3;
+    }
+    else if (cents == 4)
+    {
+        return 4;
+    }
+    return 0;
+}
